6ae9c48449d204b7c83162d1dd0c70a185f504e4
Kristoffer <kb@ristolina.com>
2019-08-08 17:27:34 +0200

    Updates

---
+++ precache-manifest.36c24891a8da7168d308f5b41491bbea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "8e671e2cdbec539999dd",
+    "url": "/robofriends/static/js/main.8e671e2c.chunk.js"
+  },
+  {
+    "revision": "ed16a08fec31cbb56ebe",
+    "url": "/robofriends/static/js/2.ed16a08f.chunk.js"
+  },
+  {
+    "revision": "8e671e2cdbec539999dd",
+    "url": "/robofriends/static/css/main.73ea7854.chunk.css"
+  },
+  {
+    "revision": "ed16a08fec31cbb56ebe",
+    "url": "/robofriends/static/css/2.784beb99.chunk.css"
+  },
+  {
+    "revision": "030e075a4e3a06601adbd1271339cb27",
+    "url": "/robofriends/index.html"
+  }
+];
